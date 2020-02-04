36aad55e5d51c885c173c42d29c2269ccd674396
serialp <cheick.guiti@gmail.com>
2020-02-04 18:05:45 +0100

    build image & run the app scripts

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker Build --tag=mylocalapp .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -p 80:80 mylocalapp
